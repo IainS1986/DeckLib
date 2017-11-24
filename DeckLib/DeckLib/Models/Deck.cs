430e4e8924971d2a5f6fdcf3b6def061f18b9ced
Iain Stanford <i_stanford@hotmail.com>
2017-11-24 19:35:43 +0000

    Lots of deck functions added, no idea if I really should have done all these but here they are. Just need the shuffle functions to go.

--- DeckLib/DeckLib/Models/Deck.cs
+++ DeckLib/DeckLib/Models/Deck.cs
@@ -29,5 +29,98 @@ namespace DeckLib.Models
         {
             Cards = cards;
         }
+
+        public void AddCard(Card c, bool blockDuplicates = false)
+        {
+            if (blockDuplicates &&
+                Cards.Any(x => x == c))
+                throw new ArgumentException(string.Format("Deck can't contain two cards of type {0}", c));
+
+            Cards.Add(c);
+        }
+
+        public bool RemoveCard(Card c)
+        {
+            if(Cards.Any(x => x == c))
+            {
+                Cards.Remove(c);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool RemoveCardAt(int i)
+        {
+            if (i >= Cards.Count)
+                return false;
+
+            Cards.RemoveAt(i);
+            return true;
+        }
+
+        public int RemoveAll(Predicate<Card> match)
+        {
+            return Cards.RemoveAll(match);
+        }
+
+        public void Clear()
+        {
+            Cards.Clear();
+        }
+
+        public void Shuffle(int count = 1)
+        {
+
+        }
+
+        public void Faro(int count = 1)
+        {
+
+        }
+
+        public Card Peek()
+        {
+            if (Cards.Count > 0)
+                return Cards[0];
+            else
+                return null;
+        }
+
+        public Card Pop()
+        {
+            Card card = null;
+            if(Cards.Count > 0)
+            {
+                card = Cards[0];
+                Cards.RemoveAt(0);
+            }
+            return card;
+        }
+
+        public void Push(Card card)
+        {
+            Cards.Add(card);
+        }
+
+        public void Deal(List<Deck> hands, int cardsPerHand = -1)
+        {
+            if (hands == null || hands.Count == 0)
+                return;
+
+            int total = Cards.Count;
+            if (cardsPerHand != -1)
+                total = cardsPerHand * hands.Count;
+
+            for(int i=0; i<total; i++)
+            {
+                if (Cards.Count == 0)
+                    break;
+
+                int hand = i % hands.Count;
+
+                hands[hand].Cards.Add(Pop());
+            }
+        }
     }
 }
